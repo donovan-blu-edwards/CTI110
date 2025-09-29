ab4b1e32388dc905067b9a0eadfde0bbbcce95a7
Donovan Edwards <edwardsd4048@student.faytechcc.edu>
2025-09-28 23:35:47 -0400

    Initial Commit

---
+++ P1LAB1_EdwardsDonovan.py
@@ -0,0 +1,6 @@
+print("Enter your first name:", end=" ")
+firstName = input()
+print("Enter your last name:", end=" ")
+lastName = input()
+
+print("Hello,", firstName, lastName + "! Welcome to CTI-110")
