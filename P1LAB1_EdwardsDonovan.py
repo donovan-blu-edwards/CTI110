fce22e2020bab3662b280323256059b96612a7df
Donovan Edwards <edwardsd4048@student.faytechcc.edu>
2025-10-14 18:46:42 -0400

    Added missing name block!

--- P1LAB1_EdwardsDonovan.py
+++ P1LAB1_EdwardsDonovan.py
@@ -1,3 +1,8 @@
+ # Donovan Edwards
+ # Sep. 28th 2025
+ # P1LAB1
+ # Simple python script that prompts the user for their first and last name and then displays a message with those variables
+
 print("Enter your first name:", end=" ")
 firstName = input()
 print("Enter your last name:", end=" ")
